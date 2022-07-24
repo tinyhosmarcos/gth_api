70173df4cb84eb766b35486ad5ef03b0c01fbb03
Yhostin Ollachica <yhostin.ollachica@ucsp.edu.pe>
2022-07-24 00:13:56 -0500

    CHORE: Add initial user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ApplicationRecord
   has_secure_token
+  has_secure_password
+
+  # associations 
+  has_many :properties, dependent: :destroy
+  has_many :status, dependent: :destroy
+
+  # validations
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+
+  enum profile_type: { homeseker: 0, landlord: 1 }
+
+  def invalidate_token
+    update(token:nil)
+  end
 end
