cfff53896315e60aa21cb94e1fbf5c09b7dee95b
Yhostin Ollachica <yhostin.ollachica@ucsp.edu.pe>
2022-07-25 12:18:59 -0500

    ADD: Create Sessions Controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  # Skip for developmet, actived for deployment
+  skip_before_action :authorize
+
+  def create
+    user = user.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
+      user.regenerate_token
+      render json: user, status: :created, except: [:password_digest, :created_at, :updated_at]
+    else
+      render json: { error: "Invalid email or password" }, status: :unauthorized
+    end
+  end
+
+  def destroy
+    current_user.invlidate_token
+    head :ok
+  end
+end
